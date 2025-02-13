3c3ce031c6e9d597ed80ee1d3a5f06800025e17b
Thales-github <thalesvitoroliveiracirino@gmail.com>
2025-02-12 21:06:23 -0300

    feat: Correção no .htaccess, um apóstrofo digitaod errado atrapalhou meu redirecionamento de url

--- roteador.php
+++ roteador.php
@@ -1,1 +1,21 @@
 <?php
+
+$pasta = $_SERVER["DOCUMENT_ROOT"] . "/desafio-revvo/";
+// require_once($pasta . "php/model/Banco_class.php");
+
+$caminho = $_SERVER["REQUEST_URI"];
+
+$vetor = explode("/", $caminho);
+$classe = $vetor[0];
+$vetor2 = explode("?", $vetor[1]);
+$metodo = $vetor[2];
+
+
+var_dump($caminho);
+var_dump($classe);
+var_dump($metodo);
+die();
+// $instancia = new Banco();
+
+$retorno = $instancia->$metodo($_REQUEST);
+echo $retorno;
